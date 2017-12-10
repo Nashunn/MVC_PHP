e68461dd7840d1efd442139318330b14f2553ca4
Nicolas BOULLET <boullet.nicolas@gmail.com>
2017-12-10 14:02:34 +0100

    Add files via upload
    Upload of the project with git

---
+++ View/home.php
@@ -0,0 +1,10 @@
+<div id="home-block">
+    <p>
+        Welcome <?php if(isset($_SESSION['user'])) echo "<b>".$_SESSION['user']."</b>"; ?> !<br>
+        This website is made with an MVC pattern.<br>
+        It also contains a bonsai.
+    </p>
+    <p>
+        - Nicolas BOULLET -
+    </p>
+</div>
